99a301fb33efc1084152ee7fea88f965b94eed89
Eric Olander <olandere@gmail.com>
2017-07-20 20:07:35 -0700

    add missing files

---
+++ app/ChordService.js
@@ -0,0 +1,41 @@
+'use strict';
+import 'whatwg-fetch';
+
+class ChordService {
+	constructor(base) {
+		this.base = base;
+	}
+	
+	serialize(data) {
+		return Object.keys(data).map(keyName => encodeURIComponent(keyName) + '=' + encodeURIComponent(data[keyName])).join('&');
+	}
+	
+	analyze(fingering, tuning) {
+		return fetch(this.base + '/analyze/' + encodeURIComponent(fingering.trim()) + "?" + this.serialize({
+      chord: encodeURIComponent(fingering.trim()),
+      tuning: tuning,
+      condense: false,
+      jazz: false
+		})).then(r => r.json());
+	}
+	
+	shellchord(fretspan, chord, tuning, condense, jazz) {
+		return fetch(this.base +'/shellchord/' + fretspan + '?' + this.serialize({
+      chord: encodeURIComponent(chord.trim()),
+      tuning: tuning,
+      condense: condense,
+      jazz: jazz
+		})
+		).then(r => r.json());
+	}
+	
+ chords(fretspan, chord, tuning, condense, jazz) {
+		return fetch(this.base +'/chords/' + fretspan + '?' + this.serialize({
+      chord: encodeURIComponent(chord.trim()),
+      tuning: tuning,
+      condense: condense,
+      jazz: jazz
+		})).then(r => r.json());
+	}
+	
+}
