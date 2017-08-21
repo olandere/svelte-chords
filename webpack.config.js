4a3998609201dd86f1e55bc7ec252e6fa194adb8
Eric Olander <olandere@gmail.com>
2017-08-20 23:05:36 -0700

    replace fetch with axios

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     compress: true,
     port: 9000
   },
-  entry: ["whatwg-fetch", "./app/main.js"],
+  entry: ["./app/main.js"],
   resolve: {
     extensions: ['.js', '.html', '.css']
   },
